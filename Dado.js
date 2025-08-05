c364d0e15f0d39f59c638ffd81085bc1a239f8e7
juan <juan@prueba.com>
2025-08-05 13:13:25 +0200

    cambios

--- Dado.js
+++ Dado.js
@@ -1,3 +1,13 @@
 export class Dado {
-    constructor()
+    constructor(numeroCaras) {
+        if(numeroCaras > 6) {
+            this.numeroCaras = 6;
+        } else {
+            this.numeroCaras = numeroCaras;
+        }
+        //this.numeroCaras = numeroCaras > 6 ? 6 : numeroCaras; 
+    }
+    tirar() {
+        return Math.floor(Math.random() * this.numeroCaras + 1);
+    }
 }
